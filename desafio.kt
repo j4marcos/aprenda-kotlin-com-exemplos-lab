99983eeba78bb1ee4ca7a740b7dd08a99723e5f1
J. Marcos <joaomarcos2004.jonh@gmail.com>
2023-09-05 23:43:44 -0300

    Complete desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,73 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
-
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
-
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
-    val inscritos = mutableListOf<Usuario>()
+data class Usuario(val nome: String, var idade: Int) {
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+val formacoes: MutableSet<Formacao> = mutableSetOf()
+val conteudos: MutableSet<ConteudoEducacional> = mutableSetOf()
+
+fun curriculo() {
+    print("curriculo de: ${this.nome} \n")
+    if (formacoes.isEmpty()) {print("não tenho formações")}
+    else {
+    print("formações: \n")
+    
+    formacoes.forEach({ it ->
+        val porcentagem = (conteudos.sumOf({it.duracao})) / (it.duracao).toFloat() * 100
+        print("${it.nome}, ${porcentagem}% \n")})
+    
+    conteudos.forEach({print("${it.nome};\n")})
+    print("\n")
+    }
+    }
+
+}
+
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60) 
+
+data class Formacao(val nome: String,val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
+    
+    val duracao: Int = conteudos.sumOf{it.duracao}
+    val inscritos: MutableList<Usuario> = mutableListOf()
+    
+    fun matricular( usuario: Usuario) {
+        inscritos.add(usuario)
+        usuario.formacoes.add(this)
+    }
+    
+    fun progresso(usuario: Usuario, cursosCompletos:Int) {
+        for (curso in 0..cursosCompletos - 1) {
+            usuario.conteudos.add(conteudos[curso])
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val cursoGit = ConteudoEducacional("Curso de git", 30)
+    val cursoHTML = ConteudoEducacional("Curso de HTML", 120)
+    val cursoCSS = ConteudoEducacional("Curso de CSS", 180)
+    var cursoJS = ConteudoEducacional("Curso de JavaScript", 180)
+    
+    var cursos = listOf(cursoGit, cursoHTML, cursoCSS, cursoJS)
+    
+    val formacaoWeb = Formacao("Formação front end",Nivel.INTERMEDIARIO, cursos)
+    
+    val fernanda = Usuario("Fernanda",23)
+    val joao = Usuario("João",19)
+    val gabriel = Usuario("gabriel",16)
+    
+    formacaoWeb.matricular(fernanda)
+    formacaoWeb.progresso(fernanda,2)
+    
+    formacaoWeb.matricular(joao)
+    formacaoWeb.progresso(joao,4)
+    
+    println(formacaoWeb)
+    println()
+    
+    fernanda.curriculo()
+    joao.curriculo()
+    gabriel.curriculo()
+    
+    
+    
 }
